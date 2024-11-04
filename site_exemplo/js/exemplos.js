519659d8745a40823718d778e79b90066d910af7
JULIA PERES CAMACHO <juperescama@gmail.com>
2024-11-04 16:58:03 -0300

    Aula 04//11/2024 - Funções JS

--- site_exemplo/js/exemplos.js
+++ site_exemplo/js/exemplos.js
@@ -1,23 +1,24 @@
 // variáveis em javascript
-let nome = "Louis" /* pode ter o valor alterado */
-const idade = 25 /* não pode ter seu valor alterado */
-var cidade = "Limeira" /* uso genérico */
+let nome = "Louis"; /* pode ter o valor alterado */
+const idade = 25; /* não pode ter seu valor alterado */
+var cidade = "Limeira"; /* uso genérico */
 
 // usando concatenação
-console.log(nome + " tem " + idade + " e mora em " + cidade)
+console.log(nome + " tem " + idade + " e mora em " + cidade);
 // usando template string
-console.log(`${nome} tem ${idade} e mora em ${cidade}`)
+console.log(`${nome} tem ${idade} e mora em ${cidade}`);
 
 //estrutura condicionais
 if (idade >= 18) {
-  console.log("É maior de idade")
+  console.log("É maior de idade");
 } else {
-  console.log("É menor de idade")
+  console.log("É menor de idade");
 }
 
 // mesmo exemplo usando operador ternário
-let resultado = idade >= 18 ? "maior de idade" : "menor de idade"
-console.log(resultado)
+let resultado =
+  idade >= 18 ? "maior de idade" : "menor de idade"; /* ? = if, : = else */
+console.log(resultado);
 
 // estrutura de repetição
 const lista_pizzas = [
@@ -27,19 +28,80 @@ const lista_pizzas = [
   "Peperoni",
   "Americana",
   "Bacon",
-]
+];
 
-console.log(lista_pizzas[4])
+console.log(lista_pizzas[4]);
 // foreach para iterar sobre a array
 lista_pizzas.forEach(function (pizza) {
-  console.log(pizza)
-})
+  console.log(pizza);
+});
 
 // exemplo de tabuada
-for(let i = 1; i<=10; i++){
-    console.log(`tabuada do ${i}`)
-    for(let x = 1; x <= 10; x++){
-        let resultado = i * x
-        console.log(`${i} x ${x} = ${resultado}`)
-    }
+for (let i = 1; i <= 10; i++) {
+  console.log(`tabuada do ${i}`);
+  for (let x = 1; x <= 10; x++) {
+    let resultado = i * x;
+    console.log(`${i} x ${x} = ${resultado}`);
+  }
+}
+
+// Uso de funções no javascript. 1- funções do javascript. 2- funções de usuários
+
+const texto = "Hoje é segunda-feira, dia";
+console.log(texto.toUpperCase());
+let palavras = texto.split("-");
+console.log(palavras);
+
+const dataUsuario = "04/11/2024";
+const itens = dataUsuario.split("/");
+/* const dataBanco = itens.reverse()*/
+const dataBanco = `${itens[2]}-${itens[1]}-${itens[0]}`;
+console.log(dataBanco);
+
+/* function dataFormatado(data, formato) {
+  // alert("A função foi chamada e recebeu como parâmetro: " + data)
+  const itens = data.split("/");
+  const datafinal = `${itens[2]}-${itens[1]}-${itens[0]} `
+  return datafinal
+} */
+
+// exemplo de função de usuario
+function dataFormatado(data, formato) {
+  if (formato == "uTOb") {
+    const itens = data.split("/");
+    const datafinal = `${itens[2]}-${itens[1]}-${itens[0]} `;
+    return datafinal;
+  }else{
+    const itens = data.split("-")
+    const datafinal = `${itens[2]}/${itens[1]}/${itens[0]} `;
+    return datafinal
+  }
+}
+
+let dataFormatoBanco = dataFormatado("22/12/2024", "uTOb");
+console.log("A data no formato banco é: " + dataFormatoBanco);
+
+let dataFormatoUsuario = dataFormatado("2024-11-09", "bTOu");
+console.log("A data no formato usuario é: " + dataFormatoUsuario);
+
+// Objeto em Javascript
+const pessoa = {
+  nome: "João", /* propriedade chave: valor */
+  idade: 25,
+  cidade: "Limeira"
+}
+
+console.log(pessoa.nome)
+
+// criar a função para fazer a troca das imagens
+function exemplo() {
+  const testeJS = document.getElementById("teste")
+  const teste2JS = document.getElementById("teste2")
+
+  // Adicionar um ouvinte
+  testeJS.addEventListener("click", function () { //função anonima
+    teste2JS.innerHTML = "Texto alterado pelo js"
+  });
+
+  // console.log(testeJS)
 }
--- site_exemplo/js/galeria.js
+++ site_exemplo/js/galeria.js
@@ -1,3 +1,18 @@
-/* alert("Oi")
-console.log("oi para o console") */
+function galeria(){
+    //Selecionar todas as imagens que estão dentro da galeria miniaturas
+    // Miniaturas sera um array de imagens
+    var miniaturas = document.querySelectorAll(".galeria_miniaturas img")
+    // console.log(miniaturas);
+   
+    miniaturas.forEach(
+        function(imagemIndividual){
+            // adicionar um ouvinte em cada uma das imagens
+            imagemIndividual.addEventListener("click"),
+                function(){
+                    
+                } 
+        }
+    )
+} 
 
+galeria()
