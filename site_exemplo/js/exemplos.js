899fc36a694fc9ddf281e3b0e0c23cddf53b30a1
jose.ifteixeira <inacio201101@gmail.com>
2024-11-04 17:24:54 -0300

    Aula 04/11/2024- criando e formatando tabelas

--- site_exemplo/js/exemplos.js
+++ site_exemplo/js/exemplos.js
@@ -38,10 +38,65 @@ pizza.forEach(
 
 for(let i = 1 ; i <= 10 ; i++){
     console.log(`Tabuada do ${i}`) 
-
     
     for(let x = 1 ;x <= 10 ; x++){
         let resultado = i * x
     console.log(`${i} x ${x} = ${resultado}`)
     }
 }
+
+// Uso de funções na javascript. 1 -Funções do Javascript . 2 - Funções de usuários
+
+const texto = "Hoje é segunda-feira, dia de estudar muito ."
+console.log(texto.toUpperCase())
+console.log(texto.toLowerCase())
+let palavras = texto.split("-")
+console.log(palavras)
+
+const dataUsuario = "04/11/2024"
+const itens = dataUsuario.split("/")
+console.log(itens)
+const DataBanco = `${itens[2]}-${itens[1]}-${itens[0]}`
+console.log(DataBanco)
+
+
+// function dataformatada(data){
+//     // alert("A função foi chamada e recebeu como paramentro : " + data)
+//     const itens = data.split("/")
+//     const datafinal = `${itens[2]}-${itens[1]}-${itens[0]}`
+//     return datafinal
+// }
+
+// let dataformatoBanco = 
+// console.log("A data  no usuario é :" + dataformatoBanco)
+
+// let dataformatoUsuario = dataformatada("22/12/2024","bTOu")
+// console.log("A data  no usuario é :" + dataformatoUsuario)
+
+
+function dataformatada(data, formato){
+    if(formato[0] == "u"){
+        const itens = data.split("/")
+        const DataBanco =`${itens[2]}-${itens[1]}-${itens[0]}`
+        return DataBanco 
+    }
+    else if(formato[0] == "b"){ 
+        const itens = data.split("-")
+        const DataBanco = `${itens[2]}/${itens[1]}/${itens[0]}`   
+        return DataBanco 
+    }
+    else{
+        return "Selecione o metodo correto ! [uTOb ou bTOu]"
+    }
+}
+ console.log(dataformatada("22/12/2024","uTOb"))
+ console.log(dataformatada("2024-12-22","bTOu"))
+
+ // objeto em Javascript
+ const pessoa= {
+    nome:"João",                //propriedade chave : valor
+    idade:25,
+    cidade:"Limeira"
+ }
+console.log(pessoa.nome)
+
--- site_exemplo/js/galeria.js
+++ site_exemplo/js/galeria.js
@@ -1,2 +1,40 @@
-// alert("oi")
-// console.log("oi para o console")
+//Criar a função para fazer a troca das imagens
+function galeria(){
+    //selecionar todas as imagens que estão dentro de galeria_miniaturas
+    //miniaturas sera um array-(grupo) de imagens
+    var miniaturas = document.querySelectorAll(".galeria_miniaturas img")
+    console.log(miniaturas)
+    miniaturas.forEach(
+        function(imagemIndividual){
+            //adicionar um ouvinte em Cada Uma das imagens
+            imagemIndividual.addEventListener("click",
+                function(){
+                    console.log(this.src)       // Exibir o caminho da imagem 
+                    document.getElementById("Foto_grande").src = this.src
+                }
+            )
+        }
+    )
+}
+
+
+
+
+
+
+
+function testeDeevento(){
+   const testeJs =document.getElementById("teste")
+   const teste2Js =document.getElementById("teste2")
+
+   //Adicionar um ouvinte
+   testeJs.addEventListener("click",
+     function(){                        //funcão anônima
+         teste2Js.innerHTML = "Texto alterado pelo javaScript"
+        }
+    )
+//   
+
+   
+}
+galeria()
