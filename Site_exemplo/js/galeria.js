86c92b0cd15c5a88312e1508336c7a3ebdfcbc24
denilson.bpinto <denilson.bp27@gmail.com>
2024-11-04 17:23:29 -0300

    Aula 04/11/2024 - Galeria com Javascript

--- Site_exemplo/js/galeria.js
+++ Site_exemplo/js/galeria.js
@@ -1,3 +1,38 @@
-// alert("Oi")
-// console.log("Oi para o console")
 
+// Função para fazer a troca das imagens
+
+function galeria(){
+    // Selecionar todas as imagens que estão dentro da classe galeria_miniaturas
+    // miniaturas será um arrey de imagens
+    var miniaturas = document.querySelectorAll(".galeria_miniatura img")
+    // console.log(miniaturas);
+    miniaturas.forEach(
+        function(imagemIndividual){
+            // Adicionar um ouvinte em cada uma das imagens
+            imagemIndividual.addEventListener("click",
+                function(){
+                    console.log(this.src)       // Exibir o caminho da imagem
+                    document.getElementById("foto_grande").src = this.src
+                }
+            )
+        }
+    )
+    
+}
+
+galeria()
+
+// Teste de evento
+
+function testeDeEvento(){
+    const testeJs = document.getElementById("teste")
+    const teste2Js = document.getElementById("teste2")
+
+    // Adicionar ouvinte
+
+    testeJs.addEventListener("click",
+        function(){                         // Função anônima
+            teste2Js.innerHTML = "Texto alterado pelo Javascript."
+        }
+    )
+}
