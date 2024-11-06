ea9e8528fc1604b81fa770f2637a51524d27bf38
JULIA PERES CAMACHO <juperescama@gmail.com>
2024-11-06 16:57:29 -0300

    Aula 06/11/2024 - Site Limeira - Pagina Inicial

--- site_exemplo/js/galeria.js
+++ site_exemplo/js/galeria.js
@@ -1,18 +1,22 @@
-function galeria(){
-    //Selecionar todas as imagens que estão dentro da galeria miniaturas
-    // Miniaturas sera um array de imagens
-    var miniaturas = document.querySelectorAll(".galeria_miniaturas img")
-    // console.log(miniaturas);
-   
-    miniaturas.forEach(
-        function(imagemIndividual){
-            // adicionar um ouvinte em cada uma das imagens
-            imagemIndividual.addEventListener("click"),
-                function(){
-                    
-                } 
-        }
-    )
-} 
+function galeria() {
+  //Selecionar todas as imagens que estão dentro da galeria miniaturas
+  // Miniaturas sera um array de imagens
+  var miniaturas = document.querySelectorAll(".galeria_miniaturas img")
+  // console.log(miniaturas)
+
+  miniaturas.forEach(function (imagemIndividual) {
+    // adicionar um ouvinte em cada uma das imagens
+    imagemIndividual.addEventListener("click"),
+      function () {
+        console.log(this.src) //Exibir o caminho da imagens
+        document.getElementById("foto_grande").src = this.src
+      }
+  })
+}
+
+//Criar a função para fazer a troca das imagens
+function testeDeEventos() {
+    
+}
 
 galeria()
