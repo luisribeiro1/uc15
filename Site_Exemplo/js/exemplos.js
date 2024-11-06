140240aa4327bb3015b1d7fdd97195c9be30642c
FELIPE GABRIEL SOUSA SILVA <felipe.gssilva2@senacsp.edu.br>
2024-11-06 17:17:06 -0300

    site limeira - página inicial - 06/11/2024

--- Site_Exemplo/js/exemplos.js
+++ Site_Exemplo/js/exemplos.js
@@ -48,3 +48,50 @@ for (let i = 1; i <= 10; i++) {
     }
 }   
 
+// Funções
+
+// 1 - Javascript
+
+const text = "Hoje é segunda-feira, dia de estudar muito."
+console.log(text.toUpperCase())
+let words = text.split(",")
+console.log(words)
+
+const dataUsuario = "04/11/2024"
+const itens = dataUsuario.split("/")
+console.log(itens);
+const dataBanco = `${itens[2]}-${itens[1]}-${itens[0]}`
+console.log(dataBanco)
+
+// 2 - Usuário
+
+function dataformatada(data, tipo){
+    // alert("A função foi chamada e recebeu como parâmetro a data: " + data)
+    if(tipo == "uTOb"){
+        const itens = data.split("/")
+        const dataFinal = `${itens[2]}-${itens[1]}-${itens[0]}`
+        return dataFinal
+    }
+    else{
+        const itens = data.split("-")
+        const dataFinal = `${itens[2]}/${itens[1]}/${itens[0]}`
+        return dataFinal
+    }
+}
+
+let dataFormatoBanco = dataformatada("22/12/2024", "uTOb")
+console.log("A data no formato banco é: " + dataFormatoBanco);
+
+let dataFormatoUsuario = dataformatada("2024-11-09", "bTOu")
+console.log("A data no formato usuário é: " + dataFormatoUsuario);
+
+// Objeto
+
+const pessoa = {
+    nome: "João",           // Propriedade chave: valor
+    idade: 25,
+    cidade: "Limeira"
+}
+
+console.log(pessoa.nome);
+
