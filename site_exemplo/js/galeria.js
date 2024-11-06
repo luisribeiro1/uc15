ccb6053dcc9c709e234c60409848afcaf3c2510f
PABLO HENRIQUE DA SILVA <pablo.hsilva9@senacsp.edu.br>
2024-11-06 17:30:44 -0300

    Site Limeira - Página Inicial 06/11/2024

--- site_exemplo/js/galeria.js
+++ site_exemplo/js/galeria.js
@@ -1,18 +1,30 @@
 //criar uma função para fazer a troca das imagens 
 
-function galeria(){
+function galeria() {
     var miniaturas = document.querySelectorAll(".galeria_miniaturas img")
-    console.log(miniaturas);
-    
+    miniaturas.forEach(
+        function (imagemIndividual) {
+            imagemIndividual.addEventListener("click",
+                function () {
+                    console.log(this.src)
+                    document.getElementById("foto_grande").src = this.src
+
+                }
+            )
+        }
+    )
+
 }
 
-function testeDeEventos(){
+galeria()
+
+function testeDeEventos() {
     const testeJs = document.getElementById("teste")
     const teste2Js = document.getElementById("teste2")
 
 
-    teste.addEventListener("click", 
-        function(){                 //função anonima
+    teste.addEventListener("click",
+        function () {                 //função anonima
             teste2Js.innerHTML = "Texto alterado pelo JavaScript"
         })
     //teste2Js.innerHTML = "Texto alterado pelo JavaScript"
