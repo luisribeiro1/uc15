4fca49ea6346b93e6d515578c1292557605ac5aa
gabriel.acpaschoal <gabrielcrivespaschoal@gmail.com>
2024-11-11 17:25:52 -0300

    Site Limeira - Página a-cidade.html - 11/11/2024

---
+++ teste/js/teste.js
@@ -0,0 +1,19 @@
+function clicando(){
+    var produto1 = document.getElementsByClassName("texto_produto1")
+  //  var produto2 = document.getElementsByClassName("texto_produto2")
+    // console.log(miniaturas)
+    console.log(produto1)
+    //console.log(produto2)
+    filho.addEventListener("click",
+        function(){
+            window.location.href = `Contato.html`;
+        }
+    )
+}
+var a = true;
+while (a == true){
+    clicando()
+}
+
+
+
